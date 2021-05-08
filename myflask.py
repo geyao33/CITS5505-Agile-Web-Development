d47cbe44c999dd629f90493f7194548ad1d12046
geyao33 <geyao33@hotmail.com>
2021-05-08 14:44:11 +0800

    main

--- myflask.py
+++ myflask.py
@@ -4,6 +4,11 @@ from flask import Flask,render_template
 
 app = Flask(__name__, instance_path=os.path.abspath(__file__))
 
+@app.route('/',methods=['GET','POST'])
+def welcome():
+    return render_template('welcome.html')
+
+
 @app.route('/index')
 def index():
     return render_template('index.html')
