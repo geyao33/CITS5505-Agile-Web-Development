02cf453c02b1e24d3e77c4b75910e1f8efef8b12
geyao33 <geyao33@hotmail.com>
2021-05-08 15:50:29 +0800

    main

--- myflask.py
+++ myflask.py
@@ -1,8 +1,9 @@
 import os
 import sqlite3
-from flask import Flask,render_template, request, redirect
+from flask import Flask,render_template, request, redirect,session
 
 app = Flask(__name__, instance_path=os.path.abspath(__file__))
+app.secret_key='yaogeandmingbozhang' #set the key for login
 
 @app.route('/',methods=['GET','POST'])
 def welcome():
@@ -11,13 +12,22 @@ def welcome():
     user = request.form.get('user') #get the username 
     pwd = request.form.get('pwd') #get the password
     if user == 'user' and pwd =="password":
+        session['user_info'] = user #save user info to session
         return redirect("/index") # jump to index if correct input
     else:
         return render_template('welcome.html',msg = 'wrong user or password')
 
 @app.route('/index')
 def index():
+    user_info = session.get('user_info')
+    if not user_info:
+        return redirect('/welcome')
     return render_template('index.html')
+
+@app.route('/logout')
+def logout():
+    del session['user_info']
+    return redirect('/welcome')
 
 if __name__ == '__main__':
     app.run()
