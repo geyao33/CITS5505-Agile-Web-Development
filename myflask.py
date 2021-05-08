20c19a2acd9187937343c8159721821ad7b290a7
geyao33 <geyao33@hotmail.com>
2021-05-08 15:25:52 +0800

    main

--- myflask.py
+++ myflask.py
@@ -1,13 +1,19 @@
 import os
 import sqlite3
-from flask import Flask,render_template
+from flask import Flask,render_template, request, redirect
 
 app = Flask(__name__, instance_path=os.path.abspath(__file__))
 
 @app.route('/',methods=['GET','POST'])
 def welcome():
-    return render_template('welcome.html')
-
+    if request.method == 'GET':
+        return render_template('welcome.html') 
+    user = request.form.get('user') #get the username 
+    pwd = request.form.get('pwd') #get the password
+    if user == 'user' and pwd =="password":
+        return redirect("/index") # jump to index if correct input
+    else:
+        return render_template('welcome.html',msg = 'wrong user or password')
 
 @app.route('/index')
 def index():
