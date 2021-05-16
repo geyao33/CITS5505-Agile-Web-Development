0288c5dfb8c6bc09afc202bab177ee2272970516
geyao33 <geyao33@hotmail.com>
2021-05-17 07:17:55 +0800

    main

--- static/js/checkAns.js
+++ static/js/checkAns.js
@@ -1,24 +1,11 @@
 var multipleAnswer = function (rightans, span, options) {
     var spanname = document.getElementById(span)
     var opt = document.getElementsByName(options)
-<<<<<<< HEAD
 
     for (i = 0; i < opt.length; i++) {
         if (opt[i].checked) {
             valuer = opt[i].value
-        } else { spanname.innerHTML = '<h4>Please enter your answer<h4>' }
-=======
-    var numofchose = 0
-    for(i = 0; i < opt.length; i++) {
-        if(opt[i].checked){
-        valuer = opt[i].value
-        numofchose = 1
-        }
->>>>>>> 68dbcde4e63f005194e33ad9ba598534ce717a51
-    }
-    
-    if(numofchose == 0) {
-        spanname.innerHTML = '<h4> Please pick an option!</h4>';
+        } else { spanname.innerHTML = '<h4>Please pick an option<h4>' }
     }
 
     var choop = document.getElementById(valuer);
@@ -31,6 +18,7 @@ var multipleAnswer = function (rightans, span, options) {
         spanname.innerHTML = '<h4> Wrong, the right answer is ' + rightans + '</h4>';
         choop.style.border = '3px solid red';
     }
+
 }
 
 var calAnswer = function (rightans, span, numQue) {
@@ -49,8 +37,3 @@ var calAnswer = function (rightans, span, numQue) {
         }
     }
 }
-
-<<<<<<< HEAD
-=======
- 
->>>>>>> 68dbcde4e63f005194e33ad9ba598534ce717a51
